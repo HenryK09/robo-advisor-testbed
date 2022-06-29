d18a433476b30f9c58f6bd82fc99c06642b2be3d
bohyun <bohyun.kang@dataknows.ai>
2022-06-29 20:58:49 +0900

    multiprocessing code 수정

--- ratestbed/run/run_process.py
+++ ratestbed/run/run_process.py
@@ -11,39 +11,41 @@ import pandas as pd
 
 def batch_product_daily(df):
     futures_list = []
-    for i in list(range(len(df))):
-        sr = df.iloc[i]
-        with futures.ProcessPoolExecutor() as executor:
+    with futures.ProcessPoolExecutor() as executor:
+        for i in list(range(len(df))):
+            sr = df.iloc[i]
             future = executor.submit(get_product_price, sr)
-            print(f'{sr} submitted')
+            print(f'{sr.algorithm_name} submitted')
             futures_list.append(future)
 
+    print('start uploading')
     result = futures.wait(futures_list)
-    for future in result.done:
+    for n, future in enumerate(result.done):
         try:
             r = future.result()
             upload_ratestbed_price(r.to_dict())
-            print(f'{r.to_dict()} uploaded')
+            print(f'{n} uploaded')
         except Exception as e:
             raise e
 
 
 def batch_product_info(df):
     futures_list = []
-    for i in list(range(len(df))):
-        sr = df.iloc[i]
-        with futures.ProcessPoolExecutor() as executor:
+    with futures.ProcessPoolExecutor() as executor:
+        for i in list(range(len(df))):
+            sr = df.iloc[i]
             future = executor.submit(get_product_info, sr)
-            print(f'{sr} submitted')
+            print(f'{sr.algorithm_name} submitted')
             futures_list.append(future)
 
+    print('start uploading')
     result = futures.wait(futures_list)
-    for future in result.done:
+    for n, future in enumerate(result.done):
         try:
             r = future.result()
             sr = data_cleansing(r)
             upload_ratestbed_info(sr.to_dict())
-            print(f'{sr.to_dict()} uploaded')
+            print(f'{n} uploaded')
         except Exception as e:
             raise e
 
