7422891413c4eaf8160c6ce87dc006e05c07f829
bohyun <bohyun.kang@dataknows.ai>
2022-06-29 20:46:40 +0900

    added indicators

--- ratestbed/run/run_process.py
+++ ratestbed/run/run_process.py
@@ -15,6 +15,7 @@ def batch_product_daily(df):
         sr = df.iloc[i]
         with futures.ProcessPoolExecutor() as executor:
             future = executor.submit(get_product_price, sr)
+            print(f'{sr} submitted')
             futures_list.append(future)
 
     result = futures.wait(futures_list)
@@ -22,6 +23,7 @@ def batch_product_daily(df):
         try:
             r = future.result()
             upload_ratestbed_price(r.to_dict())
+            print(f'{r.to_dict()} uploaded')
         except Exception as e:
             raise e
 
@@ -32,6 +34,7 @@ def batch_product_info(df):
         sr = df.iloc[i]
         with futures.ProcessPoolExecutor() as executor:
             future = executor.submit(get_product_info, sr)
+            print(f'{sr} submitted')
             futures_list.append(future)
 
     result = futures.wait(futures_list)
@@ -40,18 +43,21 @@ def batch_product_info(df):
             r = future.result()
             sr = data_cleansing(r)
             upload_ratestbed_info(sr.to_dict())
+            print(f'{sr.to_dict()} uploaded')
         except Exception as e:
             raise e
 
 
 def main():
     if os.path.isfile('ratestbed_info.xlsx'):
-        df = pd.read_excel('./ratestbed_info.xlsx', dtype=str, index_col=0)
+        df = pd.read_excel('ratestbed_info.xlsx', dtype=str, index_col=0)
     else:
         df = get_ratb_data()
-        df.to_excel('./robo-advisor-testbed/ratestbed_info.xlsx')
+        df.to_excel('ratestbed_info.xlsx')
 
+    print('start product daily process')
     batch_product_daily(df)
+    print('start product info process')
     batch_product_info(df)
 
 
